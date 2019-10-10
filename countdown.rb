556d0438df1c6dd3538be56866ed3d0ea4ade0d9
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-10 21:53:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(number)
+    while 0 < number
+      puts "#{number} SECOND(S)!"
+      number -= 1 
+    end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  sleep(5)
 end
