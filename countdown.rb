edb2470c668da841aff560a30e32b92965a00a32
William Famor <jaredfamor@gmail.com>
2019-10-18 06:41:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+number = 10
+def countdown_to_sleep
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  return "HAPPY NEW YEAR"
+  end
 end
