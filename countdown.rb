d688d7db6a8d946deb066f6990e3108b0d64d767
Nicole M Darby <nicoledarby021322@gmail.com>
2019-10-21 01:43:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
-
 def countdown
+HAPPY NEW YEAR=0
+
+while number_of_seconds < 10 
+  numbers_of_seconds-=10 
+  puts  "#{number} SECOND(S)!"
+end 
+
+puts "#{number} SECOND(S)!"
 end
